c468540aba7ba98ed19909152618a3aae3e356e9
Harsh-Agarwall <harsh.code0@gmail.com>
2025-05-18 20:39:31 +0530

    search functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,9 +4,10 @@ import StartupCard, { StartupTypeCard } from "../components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
 import { sanityFetch ,SanityLive} from "@/sanity/lib/live";
-export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {const query=(await searchParams).query
-   
-   const {data:posts}= await sanityFetch({query:STARTUPS_QUERY});
+export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
+   const query=(await searchParams).query;
+   const params={search:query||null};
+   const {data:posts}= await sanityFetch({query:STARTUPS_QUERY,params});
 
   // const posts=[
   //     {
