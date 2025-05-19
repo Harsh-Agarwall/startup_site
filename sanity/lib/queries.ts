b432250e33ad33bbed6730787d76d1e2eafb1ce6
Harsh-Agarwall <harsh.code0@gmail.com>
2025-05-19 19:05:04 +0530

    query for startup by id

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -16,3 +16,21 @@ defineQuery(`*[_type=="startup" && defined(slug.current)&& !defined($search) || 
     category
     
 }`);
+
+export const STARTUP_BY_ID_QUERY = 
+defineQuery(`*[_type=="startup" && _id==$id][0]  {
+  _id,
+    title,
+    slug,
+    _createdAt,
+    author->{
+      _id,name,username,image,bio
+    },
+    pitch,
+    views,
+    description,
+    image,
+    category,
+    pitch
+    
+}`);
